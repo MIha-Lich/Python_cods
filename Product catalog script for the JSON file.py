4820cb34ce0c8b765e708f11426a0a83a203bda1
MIha-Lich <MIhaLich132@yandex.ru>
2025-06-20 14:29:44 +0200

    Update Product catalog script for the JSON file.py
    Скрипт создания каталога товаров и запись его в CSV-файл, сохранение и дополнение данных при новом запуске.

--- Product catalog script for the JSON file.py
+++ Product catalog script for the JSON file.py
@@ -1,14 +1,15 @@
-import json
+#Скрипт создания каталога товаров и запись его в CSV-файл, сохранение и дополнение данных при новом запуске. 
+#Ограничено 3 итерациями добавления товаров (можно изменить).
+import json # Импортируем модуль json
 
-catalog = {}
-with open('test.json', 'r+') as f: 
-    reading = json.loads(f.read())
-    for line in reading: catalog[line] = int(reading[line])
-    for i in range(3):
+with open('test.json', 'r+') as f: # Открываем файл для чтения "r" и записи "+"
+    catalog = json.loads(f.read()) # Читаем файл test.json и десериализуем данные файла в формат python,
+    # в переменную catalog - каталог товаров
+    for i in range(3): # Цикл для записей новых товоров в каталог
         product = input("Продукт: ")
         int_product = int(input("Сколько: "))
-        if product in catalog: 
+        if product in catalog: # Если товар уже есть, то прибавляем количество уже к существующему товару
             catalog[str(product)] += int_product
         else: catalog[str(product)] = int_product
-    f.seek(0)
-    f.write(json.dumps(catalog))
+    f.seek(0) ) # Перенесение курсора к началу файла
+    f.write(json.dumps(catalog)) # сериализуем данные файла в формат json и Записываем в файл test.json
